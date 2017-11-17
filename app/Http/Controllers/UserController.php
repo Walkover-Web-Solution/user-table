53453067ac2d181a39aad78a0328046032a3835f
Vipin Sharma <vipin@Vipins-iMac.local>
2017-11-17 18:50:28 +0530

    testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -289,6 +289,7 @@ class UserController extends Controller {
         if (!Schema::hasTable($tableName)) {
             Schema::create($tableName, function (Blueprint $table) use ($data) {
                 $table->increments('id');
+                $table->charset = 'utf8';
                 $table->collation = 'utf8_unicode_ci';
                 foreach ($data as $key => $value) {
                     $table->string($value['name']);
