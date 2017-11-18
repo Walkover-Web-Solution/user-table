0aecdd8265aefa37acce1ca671334f853ad48bf1
Vipin Sharma <vipin@Vipins-iMac.local>
2017-11-18 16:32:17 +0530

    revert some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,10 +71,10 @@
                 @auth
                 <a href="{{ url('/tables') }}">Home</a>
                 @else
-                <!--<a href="{{env('SOCKET_LOGIN_URL')}}&redirect_uri={{env('APP_URL')}}/socketlogin">Login</a>-->
-                <a href="https://viasocket.com/login?token_required=true&redirect_uri=http://contact-crm-test.herokuapp.com/socketlogin">Login</a>
-                <!--<a href="{{env('SOCKET_SIGNUP_URL')}}&redirect_uri={{env('APP_URL')}}/socketlogin">Register</a>-->
-                <a href="https://viasocket.com/signup?token_required=true&redirect_uri=http://contact-crm-test.herokuapp.com/socketlogin">Register</a>
+                <a href="{{env('SOCKET_LOGIN_URL')}}&redirect_uri={{env('APP_URL')}}/socketlogin">Login</a>
+                <!--<a href="https://viasocket.com/login?token_required=true&redirect_uri=http://contact-crm-test.herokuapp.com/socketlogin">Login</a>-->
+                <a href="{{env('SOCKET_SIGNUP_URL')}}&redirect_uri={{env('APP_URL')}}/socketlogin">Register</a>
+                <!--<a href="https://viasocket.com/signup?token_required=true&redirect_uri=http://contact-crm-test.herokuapp.com/socketlogin">Register</a>-->
                 @endauth
             </div>
             @endif
