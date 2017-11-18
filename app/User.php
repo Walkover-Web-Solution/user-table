7f99b340e3f1fc164ec00a28c77172ba98f1629b
Sapna Bhayal <sapna.lachheta01@gmail.com>
2017-11-18 19:13:54 +0530

    table rename

--- app/User.php
+++ app/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = 'users_new';
     protected $fillable = [
         'first_name','last_name', 'email', 'password','api_token','id'
     ];
