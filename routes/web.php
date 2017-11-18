d444d1e2f0505a44c8bd7ed0b22ded1f42eca52a
Sapna Bhayal <sapna.lachheta01@gmail.com>
2017-11-18 18:12:49 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login', function () {
+    return view('welcome');
+})->name('login');
 
 Route::get('/tables', 'UserController@getUserAllTables')->name('tables');
 Route::get('socketlogin', 'LoginController@login');
 Route::get('unauthorised', function () {
     return view('unauthorised');
 })->name('unauthorised');
-
 
 # To Show table data
 Route::get('/tables/{tableName}', 'TableController@loadSelectedTable');
@@ -45,9 +44,59 @@ Route::get('/configure/{tableName}', 'TableController@loadSelectedTableStructure
 
 # for alter user table in database
 Route::post('/configureTable', 'TableController@configureSelectedTable');
+
+
+//Auth::routes();
+
+// Authentication Routes...
+//Route::get('login', [
+//  'as' => 'login',
+//  'uses' => 'Auth\LoginController@showLoginForm'
+//]);
+//Route::post('login', [
+//  'as' => '',
+//  'uses' => 'Auth\LoginController@login'
+//]);
+//Route::post('logout', [
+//  'as' => 'logout',
+//  'uses' => 'Auth\LoginController@logout'
+//]);
+//
+//// Password Reset Routes...
+//Route::post('password/email', [
+//  'as' => 'password.email',
+//  'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail'
+//]);
+//Route::get('password/reset', [
+//  'as' => 'password.request',
+//  'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm'
+//]);
+//Route::post('password/reset', [
+//  'as' => '',
+//  'uses' => 'Auth\ResetPasswordController@reset'
+//]);
+//Route::get('password/reset/{token}', [
+//  'as' => 'password.reset',
+//  'uses' => 'Auth\ResetPasswordController@showResetForm'
+//]);
+//
+//// Registration Routes...
+//Route::get('register', [
+//  'as' => 'register',
+//  'uses' => 'Auth\RegisterController@showRegistrationForm'
+//]);
+//Route::post('register', [
+//  'as' => '',
+//  'uses' => 'Auth\RegisterController@register'
+//]);
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+
+
         
-Route::get('/home/{tab}', 'HomeController@filterTab');
-Route::get('/user_data/{id}', 'UserController@getDetailsOfUserById');
+//Route::get('/home/{tab}', 'HomeController@filterTab');
+//Route::get('/user_data/{id}', 'UserController@getDetailsOfUserById');
 #serach by filters
 //Route::post('/filter', 'UserController@applyFilters');
 Route::post('/filter', 'TableController@applyFilters');
